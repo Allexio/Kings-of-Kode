91b689e6bacb66210374084f43ba75cdc14b3f95
kevin <kevin.voyer.developpeur@gmail.com>
2020-10-19 16:45:59 +0200

    Add Game and Die classes

---
+++ app/src/main/java/com/example/kingsofkode/core/Die.kt
@@ -0,0 +1,14 @@
+package com.example.kingsofkode.core
+
+class Die {
+    private var value:Int = 0
+
+    fun getValue():Int {
+        return value
+    }
+
+    fun roll() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/kingsofkode/core/Game.kt
@@ -0,0 +1,62 @@
+package com.example.kingsofkode.core
+
+class Game {
+    private var characters = mutableListOf<Character>()
+    private var dice = mutableListOf<Die>()
+    private var cards = mutableListOf<Card>()
+    private var king:Character? = null
+    private var currentPlayer:Character? = null
+    private var showedCards = mutableListOf<Card>()
+
+    fun run() {
+
+    }
+
+    fun hasWin():Boolean {
+        var count = 0
+        for (character in characters) {
+            if (character.isAlive()) {
+                count++
+            }
+        }
+        return count == 1
+    }
+
+    fun getCharacters():MutableList<Character> {
+        return characters
+    }
+
+    fun getKing():Character {
+        return king as Character
+    }
+
+    fun setKing(newKing: Character) {
+        king = newKing
+    }
+
+    fun getDice(): MutableList<Die> {
+        return dice
+    }
+
+    fun roll() {
+        for (die in dice) {
+            die.roll()
+        }
+    }
+
+    fun buyCard(character:Character, card:Card):Boolean {
+        return false
+    }
+
+    fun getState() {
+
+    }
+
+    fun getCurrentPlayer():Character {
+        return currentPlayer as Character
+    }
+
+    fun getShowedCards():MutableList<Card> {
+        return cards
+    }
+}
