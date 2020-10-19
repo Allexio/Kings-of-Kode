72ad490ab218de3254fbfc02cb5002a569e01800
kevin <kevin.voyer.developpeur@gmail.com>
2020-10-19 15:19:53 +0200

    Character methods

--- app/src/main/java/com/example/kingsofkode/core/Character.kt
+++ app/src/main/java/com/example/kingsofkode/core/Character.kt
@@ -1,5 +1,46 @@
 package com.example.kingsofkode.core
 
-class Character {
+class Character(name: String) {
+    var health = 10
+    var energy = 0
 
+    fun addCard() {
+
+    }
+
+    fun beginTurn() {
+
+    }
+
+    fun endTurn() {
+
+    }
+
+    fun isAlive():Boolean {
+        return health != 0
+    }
+
+    fun isNearToDeath():Boolean {
+        return health < 3
+    }
+
+    fun decreaseEnergy() {
+        if (energy > 1) {
+            energy--
+        }
+    }
+
+    fun increaseEnergy() {
+        energy++
+    }
+
+    fun decreaseLife() {
+        if (health > 1) {
+            health--
+        }
+    }
+
+    fun increaseLife() {
+        health++
+    }
 }
